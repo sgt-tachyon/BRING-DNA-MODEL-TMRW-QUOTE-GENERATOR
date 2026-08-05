446318ca1c343e652fc824ce4ae1b534feb67495
sgt-tachyon <kybpadilla@gmail.com>
2026-08-05 13:05:40 +0400

    Add files via upload

--- script-2.js
+++ script-2.js
@@ -1,19 +1,39 @@
 const quotes = [
     {
-    text: `Jelia: who holds in their piss here
-    Pat: Hows yall work immersion going.`,
-    image: "images/work-immer.png"
-    },
-    {
         text: `my name is jeff`,
         image: "images/jeff.jpg"
     },
+    {
+        text: `Jelia: who holds in their piss here
+        Pat: Hows yall work immersion going.`,
+        image: "images/work-immer.png"
+    },
 ];
 
-let quote = document.querySelector("#quote");
-let btn = document.querySelector("#btn");
+const quote = document.querySelector("#quote");
+const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function(){
-    let randomIndex = Math.floor(Math.random() * quotes.length);
-    quote.textContent = quotes[randomIndex];
+const popup = document.querySelector("#popup");
+const popupImg = document.querySelector("#popup-img");
+const viewPhoto = document.querySelector("#viewPhoto");
+const close = document.querySelector("#close");
+
+let currentQuote;
+
+btn.addEventListener("click", () => {
+
+    currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    quote.textContent = currentQuote.text;
+
+    viewPhoto.style.display = "block";
+});
+
+viewPhoto.addEventListener("click", () => {
+    popup.style.display = "flex";
+    popupImg.src = currentQuote.image;
+});
+
+close.addEventListener("click", () => {
+    popup.style.display = "none";
 });
